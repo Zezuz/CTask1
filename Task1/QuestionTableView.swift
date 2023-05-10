77c4da0ccb9e8c21bd25d8995197503564e63cdf
Zezuz <Mczanaty92@gmail.com>
2023-05-09 20:39:45 -0500

    Everything works and data is being passed however The tableView is not appearing

--- Task1/QuestionTableView.swift
+++ Task1/QuestionTableView.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+class QuestionTableView: NSObject, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    var questions: [Question] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell", for: indexPath) as! QuestionTableViewCell
+        let question = questions[indexPath.row]
+        cell.configure(with: question)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Handle the row selection event here.
+    }
+}
--- Task1/QuestionVC.swift
+++ Task1/QuestionVC.swift
@@ -15,6 +15,7 @@
 
 import UIKit
 
+
 class QuestionVC: UITableViewController {
     
     @IBOutlet weak var QuestionTableView: UITableView!
@@ -22,40 +23,61 @@ class QuestionVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+        
+        
+        
+        QuestionTableView.dataSource = self
+        QuestionTableView.delegate = self
+        
+       
         getQuestions { questions in
             self.questions = questions
             DispatchQueue.main.async {
-                self.tableView.reloadData()
+                // Reload the table view data after the questions are set
+                self.QuestionTableView.reloadData()
             }
         }
+        
     }
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "showQuestionSegue" {
+                let questionVC = segue.destination as! QuestionVC
+                questionVC.questions = self.questions
+            }
+        }
+
+//        getQuestions { questions in
+//            self.questions = questions
+//            DispatchQueue.main.async {
+//                self.QuestionTableView.reloadData()
+//            }
+//        }
+//    }
     
     // MARK: - Table view data source
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if segue.identifier == "showAnswerSegue" {
-                let answerVC = segue.destination as! AnswerVC
-                if let indexPath = tableView.indexPathForSelectedRow {
-                    let question = questions[indexPath.row]
-                    answerVC.Answer = question.correctAnswer
-                }
-            }
-        }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return questions.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell", for: indexPath) as! QuestionTableViewCell
-        let question = questions[indexPath.row]
-        cell.configure(with: question)
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            performSegue(withIdentifier: "showAnswerSegue", sender: self)
-        }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//            if segue.identifier == "showAnswerSegue" {
+//                let answerVC = segue.destination as! AnswerVC
+//                if let indexPath = tableView.indexPathForSelectedRow {
+//                    let question = questions[indexPath.row]
+//                    answerVC.Answer = question.correctAnswer
+//                }
+//            }
+//        }
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return questions.count
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell", for: indexPath) as! QuestionTableViewCell
+//        let question = questions[indexPath.row]
+//        cell.configure(with: question)
+//        return cell
+//    }
+//
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//            performSegue(withIdentifier: "showAnswerSegue", sender: self)
+//        }
 //    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        let question = questions[indexPath.row]
 //        let correctAnswer = question.correctAnswer
